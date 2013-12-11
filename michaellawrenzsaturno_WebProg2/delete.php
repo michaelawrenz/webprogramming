878261f0b588c8767a9af552af0706097df44154
michaelawrenz <michaellawrenz_077@yahoo.com>
2013-12-11 18:58:05 +0800

    webprogramming

---
+++ michaellawrenzsaturno_WebProg2/delete.php
@@ -0,0 +1,12 @@
+<?php 
+	/**
+	* Filename: delete.php
+	* Deleting Message
+	*/
+	include "config.php"; 
+	$id = $_GET['id']; 
+	MessageDAO::deleteMessage($id);
+	echo "<script>alert('Message successfully deleted.');window.location.href='back.php';</script>";
+ ?>
+
+
